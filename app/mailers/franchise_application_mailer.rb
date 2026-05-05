841c413767318e5679c0e27b263f8d38a9693dca
haniamariya-web <hania.mariya@nexario.services>
2026-05-05 18:17:03 +0500

    application-status-update-mail

--- app/mailers/franchise_application_mailer.rb
+++ app/mailers/franchise_application_mailer.rb
@@ -8,4 +8,20 @@ class FranchiseApplicationMailer < ApplicationMailer
       subject: "Franchise Application Payment Successful - Britain International School"
     )
   end
+
+  def application_approved(application)
+    @application = application
+    mail(
+      to: @application.email,
+      subject: "Franchise Application Approved - Britain International School"
+    )
+  end
+
+  def application_rejected(application)
+    @application = application
+    mail(
+      to: @application.email,
+      subject: "Update on your Franchise Application - Britain International School"
+    )
+  end
 end
