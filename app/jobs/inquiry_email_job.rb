4fec8f9c1969549bc6d372fa763bd11f76afb52a
haniamariya-web <hania.mariya@nexario.services>
2026-05-07 02:53:00 +0500

    remove-boilerplate-files

--- app/jobs/inquiry_email_job.rb
+++ app/jobs/inquiry_email_job.rb
@@ -2,6 +2,7 @@ class InquiryEmailJob < ApplicationJob
   queue_as :default
 
   def perform(inquiry_id)
+    # error handling missing
     inquiry = Inquiry.find(inquiry_id)
     InquiryMailer.inquiry_received(inquiry).deliver_later
     InquiryMailer.confirmation_email(inquiry).deliver_later
